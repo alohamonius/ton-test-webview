afd3dc4bfd0f391b8d4d93e762048fb937963d9d
obichuk <@gmail.com>
2024-09-23 18:30:45 +0300

    Remove python server

--- moon-project/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-MOON_DATA ='full_moons.csv'
-CACHE_DATA='moon_statistics.json'
-
-DATA_FILE_PATH = os.path.join(BASE_DIR,'data',MOON_DATA)
-CACHE_FILE_PATH= os.path.join(BASE_DIR,'data',CACHE_DATA)
