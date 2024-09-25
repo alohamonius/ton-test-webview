176e8cfb3b9e030b14c9d3e5e8813c2b35e120e8
obichuk <@gmail.com>
2024-09-25 13:57:45 +0300

    add error handler

--- src/components/3d/LunarPhaseSimulator.tsx
+++ src/components/3d/LunarPhaseSimulator.tsx
@@ -8,15 +8,31 @@ const LunarPhaseSimulator: React.FC<LunarPhaseSimulatorProps> = ({
   isVisible,
 }) => {
   useEffect(() => {
+    console.log("LunarPhaseSimulator component mounted.");
+
     if (isVisible) {
+      console.log("Showing Lunar Phase Simulator...");
+
       const script = document.createElement("script");
       script.src = "./3d/lunar-simulator/bundle.js"; //"../public/3d/lunar-simulator/bundle.js";
       script.async = true;
+
+      script.onload = () => {
+        console.log("Lunar Phase Simulator script loaded successfully.");
+      };
+
+      script.onerror = () => {
+        alert("Error loading Lunar Phase Simulator. Please try again.");
+      };
+
       document.body.appendChild(script);
 
       return () => {
+        console.log("Cleaning up Lunar Phase Simulator...");
         document.body.removeChild(script);
       };
+    } else {
+      console.log("Hiding Lunar Phase Simulator...");
     }
   }, [isVisible]);
 
